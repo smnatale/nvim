b7d436cc035bbfd76b2f83103383965dc5afeecb
smnatale <samnatale3@gmail.com>
2025-11-21 23:55:40 +0000

    telescope add hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,50 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-ui-select.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	keys = {
-		{ "<leader>sf", "<cmd>Telescope find_files<cr>" },
-		{ "<leader>sg", "<cmd>Telescope live_grep<cr>" },
-		{ "<leader>sh", "<cmd>Telescope help_tags<cr>" },
-		{ "<leader>sd", "<cmd>Telescope diagnostics<cr>" },
-		{ "<leader>sb", "<cmd>Telescope buffers<cr>" },
-	},
-	config = function()
-		local bottom_pane_config = {
-			layout_strategy = "bottom_pane",
-			layout_config = {
-				height = 0.4,
-				prompt_position = "bottom",
-			},
-			border = true,
-			sorting_strategy = "descending",
-			hidden = true,
-		}
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  keys = {
+    { "<leader>sf", "<cmd>Telescope find_files<cr>" },
+    { "<leader>sg", "<cmd>Telescope live_grep<cr>" },
+    { "<leader>sh", "<cmd>Telescope help_tags<cr>" },
+    { "<leader>sd", "<cmd>Telescope diagnostics<cr>" },
+    { "<leader>sb", "<cmd>Telescope buffers<cr>" },
+  },
+  config = function()
+    local bottom_pane_config = {
+      layout_strategy = "bottom_pane",
+      layout_config = {
+        height = 0.4,
+        prompt_position = "bottom",
+      },
+      border = true,
+      sorting_strategy = "descending",
+      hidden = true,
+    }
 
-		require("telescope").setup({
-			defaults = bottom_pane_config,
-			extensions = {
-				fzf = {},
-				["ui-select"] = {},
-			},
-		})
-		require("telescope").load_extension("fzf")
-		require("telescope").load_extension("ui-select")
-	end,
+    require("telescope").setup({
+      defaults = bottom_pane_config,
+      extensions = {
+        fzf = {},
+        ["ui-select"] = {},
+      },
+      pickers = {
+        live_grep = {
+          file_ignore_patterns = { 'node_modules', '.git' },
+          additional_args = function(_)
+            return { "--hidden" }
+          end
+        },
+        find_files = {
+          file_ignore_patterns = { 'node_modules', '.git', },
+          hidden = true
+        }
+
+      },
+    })
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("ui-select")
+  end,
 }
